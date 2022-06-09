8b5bd8ed99b7d85b88092a5d93902b86ea580a60
shiveen15 <107191263+shiveen15@users.noreply.github.com>
2022-06-09 17:59:16 +0530

    Add files via upload

---
+++ constants.ts
@@ -0,0 +1,113 @@
+export class Constants {
+    public static AppConstants = {
+        heading: 'Live Spaces',
+        subHeading: 'All NFTs on Cyber either belong to or we were minted by their space creator.',
+        buttons : [
+            {
+                buttonText: 'All',
+            },
+            {
+                buttonText: 'Latest Shows',
+            },
+            {
+                buttonText: 'Most Popular'
+            }
+        ],
+        cardDetails: [
+            {
+                image: 'https://hatrabbits.com/wp-content/uploads/2017/01/random.jpg',
+                description: 'Van Gallery',
+                tags: [
+                    {
+                        id: '@0x0f',
+                        name: '0x0feae..'
+                    }
+                ],
+                likes: 21,
+                latest: true
+            },
+            {
+                image: 'https://www.referenseo.com/wp-content/uploads/2019/03/image-attractive-960x540.jpg',
+                description: 'Teufzer',
+                tags: [
+                    {
+                        id: '@teufeurs',
+                        name: '0xd9ec..'
+                    }
+                ],
+                likes: 19,
+                latest: true
+            },
+            {
+                image: 'https://toponseek.com/blogs/wp-content/uploads/2019/06/toi-uu-hinh-anh-optimize-image.jpg',
+                description: '6529 AB + 1',
+                tags: [
+                    {
+                        id: '@6529',
+                        name: '0xfdce..'
+                    }
+                ],
+                likes: 1,
+                latest: false
+            },
+            {
+                image: 'https://www.referenseo.com/wp-content/uploads/2019/03/image-attractive-960x540.jpg',
+                description: 'Trap',
+                tags: [
+                    {
+                        id: '@abced2',
+                        name: 'Ana Shrat'
+                    }
+                ],
+                likes: 2,
+                latest: true
+            },
+            {
+                image: 'https://toponseek.com/blogs/wp-content/uploads/2019/06/toi-uu-hinh-anh-optimize-image.jpg',
+                description: 'Meta Trap House',
+                tags: [
+                    {
+                        id: '@abced2',
+                        name: 'Ana Shrat'
+                    }
+                ],
+                likes: 26,
+                latest: false
+            },
+            {
+                image: 'https://toponseek.com/blogs/wp-content/uploads/2019/06/toi-uu-hinh-anh-optimize-image.jpg',
+                description: 'Jay Walk',
+                tags: [
+                    {
+                        id: '@anaa',
+                        name: '0xcdef'
+                    }
+                ],
+                likes: 10
+            },
+            {
+                image: 'https://hatrabbits.com/wp-content/uploads/2017/01/random.jpg',
+                description: 'Madness Walk',
+                tags: [
+                    {
+                        id: '@abced2',
+                        name: 'Ana'
+                    }
+                ],
+                likes: 12
+            },
+            {
+                image: 'https://toponseek.com/blogs/wp-content/uploads/2019/06/toi-uu-hinh-anh-optimize-image.jpg',
+                description: 'Meta Trap House',
+                tags: [
+                    {
+                        id: '@abced2',
+                        name: 'Ana Shrat'
+                    }
+                ],
+                likes: 201
+            }
+        ]
+
+    }
+}
